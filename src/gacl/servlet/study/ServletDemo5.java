d1ca790017c7fcfae29bb09bbc37d49b3a42a378
jungilwoo93 <panpan9311@gmail.com>
2020-05-06 13:43:10 +0200

    demo pour stocker les caches chez client

--- src/gacl/servlet/study/ServletDemo5.java
+++ src/gacl/servlet/study/ServletDemo5.java
@@ -13,7 +13,9 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ServletDemo5")
 public class ServletDemo5 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    //在客户端缓存Servlet的输出
+	//对于不经常变化的数据，在servlet中可以为其设置合理的缓存时间值，以避免浏览器频繁向服务器发送请求，提升服务器的性能。
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -27,7 +29,11 @@ public class ServletDemo5 extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String data = "abcddfwerwesfasfsadf";
+		//设置数据合理的缓存时间值，以避免浏览器频繁向服务器发送请求，提升服务器的性能
+		//这里是将数据的缓存时间设置为1天
+		response.setDateHeader("expires",System.currentTimeMillis() + 24 *3600 * 1000);
+		response.getOutputStream().write(data.getBytes());
 	}
 
 	/**
